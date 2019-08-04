bfd1a606a98a1860f3b9e18b7eae36a007c95c4c
Mako Kamiya <mako4kamiya@gmail.com>
2019-08-04 20:06:30 +0900

    ログイン機能を実装しました

--- app.rb
+++ app.rb
@@ -32,16 +32,51 @@ end
 ###
 #ルーティン
 ##
-get '/' do
+
+# get '/index' do
+#     @posts = db.exec_params("SELECT * FROM posts")
+#     erb :index
+# end
+
+#/registerにアクセスすると、登録画面が表示される。
+get '/signup' do
+    erb :signup
+end
+
+#/registerにアクセスすると、記入した内容がデータベースに保存される。
+post '/signup' do
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    db.exec("INSERT INTO users(name, email, password) VALUES($1,$2,$3)",[name,email,password])
+    redirect '/signin'
+end
+
+get '/signin' do
+    erb :signin
+end
+
+post '/signin' do
+    name = params[:name]
+    password = params[:password]
+    puts "hello"
+    user_id = db.exec("SELECT id FROM users WHERE name = $1 AND password = $2",[name,password]).first
+    session[:user_id] = user_id['id'] #ハッシュ（id）を指定して、値(1とか)を持ってきてる。
+    redirect '/index'
+end
+
+#/index にアクセスすると、掲示板の内容一覧と投稿画面が表示される
+get '/index' do
     @posts = db.exec_params("SELECT * FROM posts")
     erb :index
 end
 
-get '/post' do #投稿画面
+get '/post' do
     erb :post
 end
 
-post '/post' do #post画面のフォームデータをDBに送る
+#/post にアクセスすると、投稿された内容がデータベースに保存される
+post '/post' do
     name = params[:name]
     email = params[:email]
     content = params[:content]
@@ -49,11 +84,4 @@ post '/post' do #post画面のフォームデータをDBに送る
     redirect '/'
 end
 
-#↓↓出来なかったやつ
-# post '/post' dogi
-#     name = params[:name]
-#     email = params[:email]
-#     content = params[:content]
-#     db.exec("INSERT INTO posts(name, email, content) VALUES(name,email,content)")
-#     redirect '/'
-# end
+
